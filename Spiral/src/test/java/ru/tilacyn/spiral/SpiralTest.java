e2b11c695356aef4016056bee400a9447d59b4bb
tilacyn <tilacyn@yandex.ru>
2017-10-31 01:13:50 +0300

    Update SpiralTest.java

--- Spiral/src/test/java/ru/tilacyn/spiral/SpiralTest.java
+++ Spiral/src/test/java/ru/tilacyn/spiral/SpiralTest.java
@@ -13,63 +13,34 @@ import static org.junit.Assert.*;
 public class SpiralTest {
     @Test
     public void printSpiral() {
-        int[][] arr = new int[3][3];
-        arr[0][0] = 7;
-        arr[0][1] = 8;
-        arr[0][2] = 9;
-        arr[1][0] = 6;
-        arr[1][1] = 1;
-        arr[1][2] = 2;
-        arr[2][0] = 5;
-        arr[2][1] = 4;
-        arr[2][2] = 3;
+        int[][] arr = {{7, 8, 9}, {6, 1, 2}, {5, 4, 3}};
         Spiral spiral = new Spiral();
         int[] res = spiral.printSpiral(arr);
-        for(int i = 0 ; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             assertEquals(res[i], i + 1);
         }
 
 
-
         arr = new int[5][5];
-        for(int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 arr[i][j] = 5 * i + j;
             }
         }
 
+        int[] expected = {12, 13, 18, 17, 16, 11, 6, 7, 8, 9,
+                14, 19, 24, 23, 22, 21, 20, 15, 10, 5, 0, 1, 2, 3, 4};
+
         res = spiral.printSpiral(arr);
-        assertEquals(12, res[0]);
-        assertEquals(13, res[1]);
-        assertEquals(18, res[2]);
-        assertEquals(17, res[3]);
-        assertEquals(16, res[4]);
-        assertEquals(11, res[5]);
-        assertEquals(6, res[6]);
-        assertEquals(7, res[7]);
-        assertEquals(8, res[8]);
-        assertEquals(9, res[9]);
-        assertEquals(14, res[10]);
-        assertEquals(19, res[11]);
-        assertEquals(24, res[12]);
-        assertEquals(23, res[13]);
-        assertEquals(22, res[14]);
-        assertEquals(21, res[15]);
-        assertEquals(20, res[16]);
-        assertEquals(15, res[17]);
-        assertEquals(10, res[18]);
-        assertEquals(5, res[19]);
-        assertEquals(0, res[20]);
-        assertEquals(1, res[21]);
-        assertEquals(2, res[22]);
-        assertEquals(3, res[23]);
-        assertEquals(4, res[24]);
+
+        assertArrayEquals(expected, res);
     }
 
     /**
      * an additional function to test sortByCols: it creates a random matrix but in the first row
      * each element is unique and in range [0, n - 1]
      * and checks whether a result matrix has a first row like [0, 1, 2, ..., n - 1]
+     *
      * @param n - square side
      */
 
@@ -80,17 +51,17 @@ public class SpiralTest {
 
         Random random = new Random();
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             notUsed.add(i);
         }
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int j = Math.abs(random.nextInt()) % notUsed.size();
             arr[0][i] = notUsed.get(j);
 
             Integer tmp = notUsed.remove(j);
         }
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 arr[i][j] = random.nextInt();
             }
         }
@@ -99,33 +70,41 @@ public class SpiralTest {
 
         int[][] res = spiral.sortByCols(arr);
 
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             assertEquals(res[0][i], i);
     }
 
     @Test
     public void sortByCols() {
-        int[][] arr = new int[3][3];
-        arr[0][0] = 3;
-        arr[0][1] = 2;
-        arr[0][2] = 1;
-        arr[1][0] = 6;
-        arr[1][1] = 5;
-        arr[1][2] = 4;
-        arr[2][0] = 9;
-        arr[2][1] = 8;
-        arr[2][2] = 7;
+        int[][] arr = {{3, 2, 1}, {6, 5, 4}, {9, 8, 7}};
+
         Spiral spiral = new Spiral();
         int[][] res = spiral.sortByCols(arr);
 
-        for(int i = 0; i < 3; i++)
-            for(int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
                 assertEquals(res[i][j], i * 3 + j + 1);
             }
 
-        for(int i = 1; i < 50; i++) {
+        for (int i = 1; i < 50; i++) {
             randomTest(i);
         }
+
+        int[][] arr1 = {{1, 1, 1}, {5, 10, 7}, {0, 3, 2}};
+
+        assertArrayEquals(arr1, spiral.sortByCols(arr1));
+
+        int[][] arr2 = {{1, -90, 1}, {100, 100, 23}, {55, 34, 0}};
+
+        int[][] res2 = {{-90, 1, 1}, {100, 100, 23}, {34, 55, 0}};
+
+        assertArrayEquals(res2, spiral.sortByCols(arr2));
+
+        int[][] arr3 = {{4000, 399, 12, -101}, {501, 502, 503, 504}, {0, 1, 89, 3}, {-1, 3, 5, 7}};
+
+        int[][] res3 = {{-101, 12, 399, 4000}, {504, 503, 502, 501}, {3, 89, 1, 0}, {7, 5, 3, -1}};
+
+        assertArrayEquals(res3, spiral.sortByCols(arr3));
 
     }
 
