a9df8710c4dad8cb0c37f9cbdc8f7491a8b0df69
Maksim Kryuchkov <tilacyn@yandex.ru>
2017-12-05 16:56:18 +0300

    New branch for HashTable++ created from branch HashTable, old deleted. Only tests added to commit

--- HashTable/src/test/java/ru/tilacyn/hashTable/HashTableTest.java
+++ HashTable/src/test/java/ru/tilacyn/hashTable/HashTableTest.java
@@ -2,19 +2,21 @@ package ru.tilacyn.hashTable;
 
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 /**
  * a test class for HashTable
  */
+public class HashTableTest {
 
-public class HashTableTest{
     @Test
     public void clear() {
         HashTable ht = new HashTable(4);
         ht.put("1", "2");
         ht.clear();
-        assert !(ht.contains("1"));
+        assertFalse(ht.contains("1"));
         assertEquals(ht.size(), 0);
     }
 
@@ -27,9 +29,53 @@ public class HashTableTest{
     @Test
     public void getHash() {
         HashTable ht = new HashTable(100);
-        assertEquals(ht.getHash("1"), ht.getHash("1"));
-        assert ht.getHash("2") < 100 && ht.getHash("2") >= 0;
+        assertTrue(ht.getHash("1") == ht.getHash("1"));
+        assertTrue(ht.getHash("2") < 100 && ht.getHash("2") >= 0);
     }
+
+    public String makeRandomString() {
+        int n;
+        do {
+            n = Math.abs(new Random().nextInt()) % 8;
+        } while (n == 0);
+        char[] arr = new char[n];
+
+        for (int i = 0; i < n; i++) {
+            int number = Math.abs(new Random().nextInt());
+            if (number % 4 == 0) {
+                arr[i] = 'a';
+            }
+            else if (number % 4 == 1) arr[i] = 'b';
+            else if (number % 4 == 2) arr[i] = 'c';
+            else if (number % 4 == 3) arr[i] = 'd';
+        }
+        return arr.toString();
+    }
+
+    /**
+     * additional test function on processing collisions
+     * it makes sure that there ARE collisions and checks whether everything's alright
+     */
+
+    public void processCollisions() {
+        HashTable ht = new HashTable(10);
+        for (int i = 0; i < 20; i++) {
+            String key1 = makeRandomString();
+            String key2 = makeRandomString();
+            if (key1.hashCode() % 10 == key2.hashCode() % 10) {
+                assertEquals(ht.put(key1, "1"), null);
+                assertEquals(ht.put(key2, "2"), null);
+                assertTrue(ht.contains(key1));
+                assertTrue(ht.contains(key2));
+                assertEquals(ht.get(key1), "1");
+                assertEquals(ht.get(key2), "2");
+            }
+        }
+    }
+
+    /**
+     * processing collisions added
+     */
 
     @Test
     public void put() {
@@ -41,6 +87,8 @@ public class HashTableTest{
         assertEquals(ht.get("5"), "6");
         assertEquals(ht.put("3", "10"), "4");
         assertEquals(ht.get("3"), "10");
+
+        processCollisions();
     }
 
     @Test
@@ -57,12 +105,12 @@ public class HashTableTest{
     @Test
     public void contains() {
         HashTable ht = new HashTable(10);
-        assert !(ht.contains("1"));
+        assertFalse(ht.contains("1"));
         ht.put("1", "2");
         ht.put("3", "4");
-        assert (ht.contains("1"));
+        assertTrue(ht.contains("1"));
         ht.remove("1");
-        assert !(ht.contains("1"));
+        assertFalse(ht.contains("1"));
     }
 
     @Test
@@ -88,4 +136,33 @@ public class HashTableTest{
         ht.remove("3");
         assertEquals(ht.size(), 2);
     }
+
+    /**
+     * new test function added consequently
+     */
+
+
+    @Test
+    public void rebuild() {
+        HashTable ht = new HashTable(4);
+        ht.put("1", "1");
+        ht.put("2", "2");
+        ht.put("3", "3");
+        ht.put("4", "4");
+        ht.put("5", "5");
+        ht.put("6", "6");
+        ht.put("7", "7");
+        ht.rebuild();
+
+        assertEquals(ht.get("1"), "1");
+        assertEquals(ht.get("2"), "2");
+        assertEquals(ht.get("3"), "3");
+        assertEquals(ht.get("4"), "4");
+        assertEquals(ht.get("5"), "5");
+        assertEquals(ht.get("6"), "6");
+        assertEquals(ht.get("7"), "7");
+        assertEquals(ht.get("8"), null);
+        assertEquals(ht.get("9"), null);
+    }
+
 }
--- HashTable/src/test/java/ru/tilacyn/list/ListTest.java
+++ HashTable/src/test/java/ru/tilacyn/list/ListTest.java
@@ -1,35 +1,96 @@
 package ru.tilacyn.list;
 
-import org.junit.Assert.*;
-
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
 
 
+public class ListTest {
+    @Test
+    public void size() throws Exception {
+        List l = new List();
+        assertEquals(0, l.size());
+        l.add("a", "b");
+        assertEquals(1, l.size());
+        l.add("a", "b");
+        assertEquals(2, l.size());
+        l.add("a", "b");
+        assertEquals(3, l.size());
+        l.add("a", "b");
+        assertEquals(4, l.size());
+        l.add("a", "b");
+        assertEquals(5, l.size());
+        l.remove("b");
+        assertEquals(5, l.size());
+        l.remove("a");
+        assertEquals(4, l.size());
+        l.add("s", "t");
+        assertEquals(5, l.size());
 
-public class ListTest{
+    }
 
     @Test
-    public void makeList() {
+    public void isEmpty() throws Exception {
         List l = new List();
-        assertEquals(l.head, null);
+        assertTrue(l.isEmpty());
+        l.add(" ", " ");
+        assertFalse(l.isEmpty());
+        l.remove(" ");
+        assertTrue(l.isEmpty());
+        l.add("hello", "bye");
+        assertFalse(l.isEmpty());
+        l.remove(" ");
+        assertFalse(l.isEmpty());
+        l.remove("hello");
+        assertTrue(l.isEmpty());
+    }
+
+    @Test
+    public void getHeadKey() throws Exception {
+        List l = new List();
+        assertEquals(null, l.getHeadKey());
+        l.add("a", " ");
+        assertEquals("a", l.getHeadKey());
+        l.add("b", " ");
+        assertEquals("b", l.getHeadKey());
+        l.add("a", " ");
+        assertEquals("a", l.getHeadKey());
+        l.add(" ", " ");
+        assertEquals(" ", l.getHeadKey());
+        l.add("Lamerica", " ");
+        assertEquals("Lamerica", l.getHeadKey());
+        l.remove("Lamerica");
+        assertEquals(" ", l.getHeadKey());
     }
 
     @Test
     public void add() {
         List l = new List();
         l.add("1", "2");
-        assertEquals(l.head.key, "1");
-        assertEquals(l.head.s, "2");
-        assertEquals(l.head.prev, null);
-        assertEquals(l.head.next, null);
-        assertEquals(l.head.next, null);
-        l.add("3", "4");
-        assertEquals(l.head.prev, null);
-        assertEquals(l.head.s, "4");
-        assertEquals(l.head.key, "3");
-        assertEquals(l.head.next.key, "1");
+        l.add("2", "!");
+        assertTrue(l.contains("1"));
+        assertTrue(l.contains("2"));
+        assertFalse(l.contains("!"));
+        assertFalse(l.contains("3"));
+
+
+        l.add("1", "2");
+        l.add("2", "!");
+        assertTrue(l.contains("1"));
+        assertTrue(l.contains("2"));
+        assertFalse(l.contains("!"));
+        assertFalse(l.contains("3"));
+
+        l.add("-1", "-1");
+        l.add("-1", "-1");
+        l.add("100", "-1");
+
+        assertTrue(l.contains("100"));
+        assertTrue(l.contains("-1"));
+        assertFalse(l.contains("3"));
+        assertFalse(l.contains("0"));
+        assertTrue(l.contains("1"));
+        assertTrue(l.contains("2"));
     }
 
     @Test
@@ -37,22 +98,21 @@ public class ListTest{
         List l = new List();
         l.add("1", "2");
         l.remove("1");
-        assertEquals(l.head, null);
         l.add("3", "4");
         l.add("5", "6");
         l.remove("3");
-        assertEquals(l.head.key, "5");
+
     }
 
     @Test
     public void contains() {
         List l = new List();
         l.add("1", "2");
-        assert l.contains("1");
+        assertTrue(l.contains("1"));
         l.add("3", "4");
-        assert l.contains("3");
+        assertTrue(l.contains("3"));
         l.remove("1");
-        assert !(l.contains("1"));
+        assertFalse(l.contains("1"));
     }
 
     @Test
