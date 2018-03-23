1642901ac54c19039c096a86250db86045308bd5
Maksim Kryuchkov <tilacyn@yandex.ru>
2018-03-23 05:46:20 +0300

    OX: first attempt

---
+++ OX/src/sample/Controller.java
@@ -0,0 +1,153 @@
+package sample;
+
+/**
+ * a controller that provides class connection
+ */
+public class Controller {
+    private Logic logic;
+    private GameLayout gameLayout;
+    private MainLayout mainLayout;
+    private Main main;
+
+    private boolean bot = false;
+
+    private int XWins = 0;
+    private int OWins = 0;
+    private int draws = 0;
+
+
+    /**
+     * starts a new hot seat game
+     */
+    public void start() {
+        bot = false;
+        resetField();
+        logic.resetField();
+        main.startGame();
+    }
+
+    /**
+     * continues current game
+     */
+    public void continueGame() {
+        main.startGame();
+    }
+
+    /**
+     * starts a new game with bot
+     */
+    public void startWithBot() {
+        bot = true;
+        resetField();
+        logic.resetField();
+        main.startGame();
+    }
+
+    /**
+     * returns back to the main scene from game scene
+     */
+    public void backToMain() {
+        main.backToMain();
+    }
+
+    /**
+     * resets only the field that is shown in UI
+     */
+    public void resetField() {
+        gameLayout.redraw();
+    }
+
+    /**
+     * processes pressing on the exact button
+     * @param i - button row number
+     * @param j - button column number
+     */
+    public void press(int i, int j) {
+        if (!bot) {
+            logic.press(i, j);
+        } else {
+            logic.pressWithBot(i, j);
+        }
+    }
+
+    /**
+     * draws changes that happened after a move
+     * @param i row number
+     * @param j column number
+     * @param currentPlayer player that made a move number
+     */
+    public void draw(int i, int j, int currentPlayer) {
+        gameLayout.draw(i, j, currentPlayer);
+    }
+
+    /**
+     * binds this controller to the Logic object
+     * @param logic Logic object
+     */
+    public void setLogic(Logic logic) {
+        this.logic = logic;
+    }
+
+    /**
+     * binds this controller to the GameLayout object
+     * @param gameLayout GameLayout object
+     */
+    public void setGameLayout(GameLayout gameLayout) {
+        this.gameLayout = gameLayout;
+    }
+
+    /**
+     * binds this controller to the MainLayout object
+     * @param mainLayout MainLayout object
+     */
+    public void setMainLayout(MainLayout mainLayout) {
+        this.mainLayout = mainLayout;
+    }
+
+    /**
+     * binds this controller to the Main object
+     * @param main Main object
+     */
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    /**
+     * changes the statistics according to the last game result
+     * @param winner 1 if X won, 2 if O won, -1 if draw occurred
+     */
+    public void addStatistics(int winner) {
+        if (winner == 1) {
+            XWins++;
+        } else if (winner == 2) {
+            OWins++;
+        } else {
+            draws++;
+        }
+        mainLayout.addStatistics();
+    }
+
+    /**
+     * returns number of X wins
+     * @return number of X winds
+     */
+    public int getXWins() {
+        return XWins;
+    }
+
+    /**
+     * returns number of O wins
+     * @return number of O wins
+     */
+    public int getOWins() {
+        return OWins;
+    }
+
+    /**
+     * returns number of draws
+     * @return number of draws
+     */
+    public int getDraws() {
+        return draws;
+    }
+}
